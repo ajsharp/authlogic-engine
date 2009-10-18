142873b5cd0a46a740cf184efb092c5245ee074a
Alex Sharp <ajsharp@gmail.com>
2009-10-18 18:59:22 -0500

    user model now only requires an email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   
-  acts_as_authentic
-  
-  validates_presence_of :email
+  acts_as_authentic do |u|
+    u.validate_email_field
+    u.validate_login_field false
+  end
   
   def deliver_password_reset_instructions!  
     reset_perishable_token!  
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,31 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe User do
-  include UserSim
-  
-  describe 'validations' do
-    
-    it 'should create a new instance given valid attributes' do
-      User.create!(valid_user_attributes).should be_true
+  before(:all) do
+    @valid_attributes = { 
+      :email    => "user@example.com",
+      :password => "password",
+      :password_confirmation => "password"
+    }
+  end
+  context "validations" do
+    before(:each) do 
+      @user = User.new
     end
-    
-  end
-  
-  describe 'behaviors' do
-    
-    it 'should acts as authentic (authlogic)' do; end
-    
-  end
 
-  
+    it "should require an email address" do
+      @user.attributes = @valid_attributes.except(:email)
+      @user.should_not be_valid
+      @user.email = @valid_attributes[:email]
+      @user.should be_valid
+    end
+
+    it "should require a password" do
+      User.new(@valid_attributes.except(:password)).should_not be_valid
+    end
+
+    it "should require a password confirmation" do
+      User.new(@valid_attributes.except(:password_confirmation)).should_not be_valid
+    end
+  end
 end
